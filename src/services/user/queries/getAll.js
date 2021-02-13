47723fd98c168e6178dd361be6bec450386f022c
abrehamgezahegn <gezahegnabereham@gmail.com>
2021-02-13 23:38:53 +0300

    (chore) added log

--- src/services/user/queries/getAll.js
+++ src/services/user/queries/getAll.js
@@ -1,6 +1,7 @@
 const User = require("../../../database/models/userModel");
 
 exports.GetAll = async (args) => {
+  console.log("asking for all users");
   const users = await User.query();
   console.log("users ", users);
   return users;
